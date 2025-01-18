32243b5d93756f4fb207e8afd86d0c4bf2fb8ad3
miranda-beatriz <miranda.beatrizcosta@gmail.com>
2025-01-18 19:56:42 -0300

    change

--- vite.config.js
+++ vite.config.js
@@ -8,16 +8,10 @@ export default defineConfig({
     outDir: "../dist",
     rollupOptions: {
       input: {
-<<<<<<< HEAD
         main: resolve(__dirname, "src/index.html"),
         cart: resolve(__dirname, "src/cart/index.html"),
         checkout: resolve(__dirname, "src/checkout/index.html"),
         
-=======
-        main:    resolve(__dirname, "src/index.html"),
-        cart:    resolve(__dirname, "src/cart/index.html"),
-        product: resolve(__dirname, "src/product_pages/index.html"),
->>>>>>> 508cd9840bb44c53d5aecccd368f3faea001a87a
       },
     },
   },
